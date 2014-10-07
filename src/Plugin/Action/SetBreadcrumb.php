484d20dc6e76015164e9fdb91ddb92b09ded98e9
Kalman Hosszu <hosszu.kalman@gmail.com>
2014-10-07 13:48:45 +0200

    Add the base of the action.

---
+++ src/Plugin/Action/SetBreadcrumb.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\rules\Plugin\Action\SetBreadcrumb.
+ */
+
+namespace Drupal\rules\Plugin\Action;
+
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\rules\Engine\RulesActionBase;
