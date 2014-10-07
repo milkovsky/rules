9571eb4e771149915c2d610f6ce2a138a39c580d
Kalman Hosszu <hosszu.kalman@gmail.com>
2014-10-07 13:52:54 +0200

    Create 'Set breadcrump' actions's definition.

--- src/Plugin/Action/SetBreadcrumb.php
+++ src/Plugin/Action/SetBreadcrumb.php
@@ -9,3 +9,27 @@ namespace Drupal\rules\Plugin\Action;
 
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\rules\Engine\RulesActionBase;
+
+/**
+ * Provides a 'Set breadcrumb' action.
+ *
+ * @Action(
+ *   id = "rules_breadcrumb_set",
+ *   label = @Translation("Set breadcrumb"),
+ *   context = {
+ *     "titles" = @ContextDefinition("string",
+ *       label = @Translation("Titles"),
+ *       description = @Translation("A list of titles for the breadcrumb links."),
+ *       multiple = TRUE
+ *     ),
+ *     "paths" = @ContextDefinition("string",
+ *       label = @Translation("Paths"),
+ *       description = @Translation("A list of Drupal paths for the breadcrumb links, matching the order of the titles."),
+ *       multiple = TRUE
+ *     )
+ *   }
+ * )
+ *
+ * @todo: Add access callback information from Drupal 7.
+ * @todo: Add group information from Drupal 7.
+ */
