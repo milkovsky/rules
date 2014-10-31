469fd08eb91102a64bb33fefc361993c3be3dfe2
Kalman Hosszu <hosszu.kalman@gmail.com>
2014-10-31 15:05:02 +0100

    Implemetn SystemSetBreadcrumb class's contruct.

--- src/Plugin/Action/SetBreadcrumb.php
+++ src/Plugin/Action/SetBreadcrumb.php
@@ -33,3 +33,19 @@ use Drupal\rules\Engine\RulesActionBase;
  * @todo: Add access callback information from Drupal 7.
  * @todo: Add group information from Drupal 7.
  */
+class SystemSetBreadcrumb extends RulesActionBase {
+
+  /**
+   * Constructs a SystemSetBreadcrumb object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+}
