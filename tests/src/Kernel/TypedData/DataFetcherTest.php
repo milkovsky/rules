d9bd13b536b9d2058e1168f77083b5abd3c58c7a
milkovsky <a.milkovsky@gmail.com>
2015-12-11 17:23:17 +0100

    #2632582: Added tests for DataFetcher.

--- tests/src/Kernel/TypedData/DataFetcherTest.php
+++ tests/src/Kernel/TypedData/DataFetcherTest.php
@@ -7,7 +7,8 @@
 
 namespace Drupal\Tests\rules\Kernel\TypedData;
 
-use \Drupal\KernelTests\KernelTestBase;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\Core\Entity\Plugin\DataType\EntityAdapter;
 
 /**
  * Class DataFetcherTest.
@@ -33,6 +34,13 @@ class DataFetcherTest extends KernelTestBase {
   protected $node;
 
   /**
+   * An entity type manager used for testing.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
    * Modules to enable.
    *
    * @var array
@@ -46,16 +54,20 @@ class DataFetcherTest extends KernelTestBase {
     parent::setUp();
     $this->typedDataManager = $this->container->get('typed_data_manager');
 
-    $entity_type_manager = $this->container->get('entity_type.manager');
-    $entity_type_manager->getStorage('node_type')
+    $this->entityTypeManager = $this->container->get('entity_type.manager');
+    $this->entityTypeManager->getStorage('node_type')
       ->create(['type' => 'page'])
       ->save();
 
-    $this->node = $entity_type_manager->getStorage('node')
+    $this->node = $this->entityTypeManager->getStorage('node')
       ->create([
         'title' => 'test',
         'type' => 'page',
       ]);
+
+    $this->installSchema('system', ['sequences']);
+    $this->installEntitySchema('user');
+    $this->installEntitySchema('node');
   }
 
   /**
@@ -68,6 +80,87 @@ class DataFetcherTest extends KernelTestBase {
       ->fetchByPropertyPath($this->node->getTypedData(), 'title.0.value')
       ->getValue()
     );
+  }
+
+  /**
+   * @cover fetchBySubPaths
+   */
+  public function testFetchingByBasicSubPath() {
+    $this->assertEquals(
+      $this->node->title->value,
+      $this->typedDataManager->getDataFetcher()
+      ->fetchBySubPaths($this->node->getTypedData(), array('title', '0', 'value'))
+      ->getValue()
+    );
+  }
+
+  /**
+   * @cover fetchByPropertyPath
+   */
+  public function testFetchingEntityReference() {
+    $user = $this->entityTypeManager->getStorage('user')
+      ->create([
+        'name' => 'test',
+        'type' => 'user',
+      ]);
+    $this->node->uid->target_id = $user->id();
+
+    $fetched_user = $this->typedDataManager->getDataFetcher()
+      ->fetchByPropertyPath($this->node->getTypedData(), 'uid.entity.value')
+      ->getValue();
+    $this->assertEquals(TRUE, $fetched_user instanceof EntityAdapter);
+  }
+
+  /**
+   * @cover fetchByPropertyPath
+   */
+  public function testFetchingEntityReferenceAtPosition0() {
+    $user = $this->entityTypeManager->getStorage('user')
+      ->create([
+        'name' => 'test',
+        'type' => 'user',
+      ]);
+    $this->node->uid->target_id = $user->id();
+
+    $fetched_user = $this->typedDataManager->getDataFetcher()
+      ->fetchByPropertyPath($this->node->getTypedData(), 'uid.0.entity.value')
+      ->getValue();
+    $this->assertEquals(TRUE, $fetched_user instanceof EntityAdapter);
+  }
+
+  /**
+   * @cover fetchByPropertyPath
+   */
+  public function testFetchingEntityReferenceAtPosition1() {
+    $user1 = $this->entityTypeManager->getStorage('user')
+      ->create([
+        'name' => 'test1',
+        'type' => 'user',
+      ]);
+    $users[]['target_id'] = $user1->id();
+    $user2 = $this->entityTypeManager->getStorage('user')
+      ->create([
+        'name' => 'test2',
+        'type' => 'user',
+      ]);
+    $users[]['target_id'] = $user2->id();
+    $this->node->uid->setValue($users);
+
+    $fetched_user = $this->typedDataManager->getDataFetcher()
+      ->fetchByPropertyPath($this->node->getTypedData(), 'uid.1.entity.value')
+      ->getValue();
+    $this->assertEquals(TRUE, $fetched_user instanceof EntityAdapter);
+  }
+
+  /**
+   * @cover fetchByPropertyPath
+   */
+  public function testFetchingNonExistingEntityReference() {
+    $this->setExpectedException('Drupal\Core\TypedData\Exception\MissingDataException');
+    $fetched_user = $this->typedDataManager->getDataFetcher()
+      ->fetchByPropertyPath($this->node->getTypedData(), 'uid.0.entity.value')
+      ->getValue();
+    $this->assertEquals(TRUE, $fetched_user instanceof EntityAdapter);
   }
 
 }
