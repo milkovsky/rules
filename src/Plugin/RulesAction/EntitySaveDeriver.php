889098a207aac56504b8027decb8bec0cbdb7020
milkovsky <a.milkovsky@gmail.com>
2016-04-11 17:36:53 +0200

    Removed unnecessary file doc comment.

--- src/Plugin/RulesAction/EntitySaveDeriver.php
+++ src/Plugin/RulesAction/EntitySaveDeriver.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\rules\Plugin\RulesAction\EntitySaveDeriver.
- */
-
 namespace Drupal\rules\Plugin\RulesAction;
 
 use Drupal\Component\Plugin\Derivative\DeriverBase;
